20a97536549f576f98b403cc8051f389fb070ac7
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-02-08 15:58:31 -0200

    Created rcc for BottomNav and PromotionBox

---
+++ src/components/Footer/PromotionBox/PromotionBox.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class PromotionBox extends Component {
+  render() {
+    return (
+      <div>
+        PromotionBox
+      </div>
+    )
+  }
+}
