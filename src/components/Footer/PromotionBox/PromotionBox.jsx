55fa35d76d36ec063bc806b801523665e28cb48a
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-02-09 18:48:27 -0200

    Basic PromotionBox sructure

--- src/components/Footer/PromotionBox/PromotionBox.jsx
+++ src/components/Footer/PromotionBox/PromotionBox.jsx
@@ -1,12 +1,26 @@
 // @flow
 import React, { Component } from 'react'
+import Card from "@material-ui/core/Card";
+
+import styles from './PromotionBox.module.css';
 
 export default class PromotionBox extends Component {
   render() {
     return (
-      <div>
-        PromotionBox
-      </div>
-    )
+      <section className={styles.Container}>
+
+        <h3 className={styles.Header}>Escolha um de nossos planos</h3>
+
+        <div className={styles.CardBox}>
+          <Card className={styles.Card}>Card1</Card>
+
+          <Card className={styles.Card}>Card2</Card>
+
+          <Card className={styles.Card}>Card3</Card>        
+        </div>
+
+
+      </section>
+    );
   }
 }
